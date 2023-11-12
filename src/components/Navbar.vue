721d9b4e7065c5645a251fc298d9016185f84a92
KyawZin <hellfirewarrior123@gmail.com>
2023-11-13 00:25:09 +0630

    kzt

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,35 +1,78 @@
 <template>
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Vue Router</span>
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <div class="container flex flex-wrap justify-between items-center mx-auto" bis_skin_checked="1">
+            <a href="/" class="flex items-center">
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Pinia</span>
             </a>
-            <button data-collapse-toggle="navbar-default" type="button"
-                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="navbar-default" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M1 1h15M1 7h15M1 13h15" />
-                </svg>
-            </button>
-            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <div class="flex md:order-2" bis_skin_checked="1">
+                <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
+                    aria-expanded="false"
+                    class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
+                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                    <span class="sr-only">Search</span>
+                </button>
+                <div class="hidden relative md:block" bis_skin_checked="1">
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none" bis_skin_checked="1">
+                        <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="sr-only">Search icon</span>
+                    </div>
+                    <input type="text" id="search-navbar" v-model="moviesStore.query"
+                        class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Search..." />
+                </div>
+                <button data-collapse-toggle="navbar-search" type="button"
+                    class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    aria-controls="navbar-search" aria-expanded="false">
+                    <span class="sr-only">Open menu</span>
+                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+            </div>
+            <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="navbar-search"
+                bis_skin_checked="1">
+                <div class="relative mt-3 md:hidden" bis_skin_checked="1">
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none" bis_skin_checked="1">
+                        <svg class="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <input type="text" id="search-navbar" v-model="moviesStore.query"
+                        class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Search..." />
+                </div>
                 <ul
-                    class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
-                        <RouterLink to="/"
-                            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                            aria-current="page">Home</RouterLink>
+                        <router-link :to="{ name: 'Home' }"
+                            class="block py-2 pr-4 pl-3 text-slate-700 rounded md:bg-transparent md:p-0 dark:text-white"
+                            active-class="text-blue-500 dark:text-blue-500" aria-current="page">Home</router-link>
                     </li>
                     <li>
-                        <RouterLink to="/about"
-                            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                            aria-current="page">About</RouterLink>
+                        <router-link :to="{ name: 'About' }"
+                            class="block py-2 pr-4 pl-3 text-slate-700 rounded md:bg-transparent md:p-0 dark:text-white"
+                            active-class="text-blue-500 dark:text-blue-500" aria-current="page">About</router-link>
                     </li>
                     <li>
-                        <RouterLink to="/movies"
-                            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                            aria-current="page">Movie</RouterLink>
+                        <router-link :to="{ name: 'MovieList' }"
+                            class="block py-2 pr-4 pl-3 text-slate-700 rounded md:bg-transparent md:p-0 dark:text-white"
+                            active-class="text-blue-500 dark:text-blue-500" aria-current="page">Movies</router-link>
                     </li>
                 </ul>
             </div>
@@ -38,8 +81,13 @@
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
+import { watchEffect } from 'vue';
+import { useMoviesStore } from '../stores/movies'
 
+const moviesStore = useMoviesStore();
+watchEffect(() => {
+    moviesStore.getMovies(moviesStore.query)
+})
 
 </script>
 
--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -2,27 +2,33 @@
     <div>
         Movies
     </div>
-    <div class="max-w-sm mx-auto" v-if="isLoading">
+    <div class="max-w-sm mx-auto" v-if="moviesStore.isLoading">
         <span class="text-2xl font-bold text-indigo-700">Is Loading...</span>
     </div>
     <div class="grid grid-cols-3 gap-4" v-else>
-        <MovieCard v-for="movie in movieList" :key="movie.id" :movie="movie" />
+        <MovieCard v-for="movie in moviesStore.movies" :key="movie.id" :movie="movie" />
     </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
+import { useMoviesStore } from '../stores/movies'
 import MovieCard from "../components/MovieCard.vue";
 
-const movieList = ref([]);
-const isLoading = ref(true);
-
-onMounted(async() => {
-    const result = await fetch('http://localhost:8000/movies')
-    const response = await result.json();
-        movieList.value = response;
-        isLoading.value = false;
+const moviesStore = useMoviesStore();
+onMounted(() => {
+    moviesStore.getMovies();
 })
+
+// const movieList = ref([]);
+// const isLoading = ref(true);
+
+// onMounted(async() => {
+//     const result = await fetch('http://localhost:8000/movies')
+//     const response = await result.json();
+//         movieList.value = response;
+//         isLoading.value = false;
+// })
 </script>
 
 <style lang="scss" scoped></style>
