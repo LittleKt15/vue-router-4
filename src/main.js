a92e8c3f1963be1f7573f4906e6d399bac5e73a1
KyawZin <hellfirewarrior123@gmail.com>
2023-11-14 01:17:13 +0630

    kzt

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import { createApp } from 'vue'
+import { markRaw, createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './routes'
 import './style.css'
 import App from './App.vue'
 
 const pinia = createPinia()
+
+pinia.use(({ store }) => {
+    store.router = markRaw(router)
+})
+
 const app = createApp(App)
 
 app.use(pinia)
--- src/stores/movies.js
+++ src/stores/movies.js
@@ -5,9 +5,18 @@ export const useMoviesStore = defineStore('movies', {
         movies: [],
         isLoading: false,
         query: '',
+        singleMovie: {},
+        year: 2000
     }),
     getters: {
-
+        totalMovies() {
+            return this.filterMovies.length;
+        },
+        filterMovies() {
+            return this.movies.filter((movie) => {
+                return movie.year >= this.year;
+            })
+        }
     },
     actions: {
         async getMovies() {
@@ -17,5 +26,15 @@ export const useMoviesStore = defineStore('movies', {
             this.movies = response;
             this.isLoading = false;
         },
+        async getSingleMovie(id) {
+            this.isLoading = true;
+            const result = await fetch(`http://localhost:8000/movies/${parseInt(id)}`);
+            if (result.status === 404) {
+                this.router.push({ name: 'NotFound' });
+            }
+            const response = await result.json();
+            this.singleMovie = response;
+            this.isLoading = false;
+        }
     },
 })
